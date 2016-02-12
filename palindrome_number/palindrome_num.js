517a051684d587a2907ab312674d28231fcce882
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-12 21:44:20 +0900

    refactoring
    use namespace

--- palindrome_number/palindrome_num.js
+++ palindrome_number/palindrome_num.js
@@ -2,12 +2,17 @@ String.prototype.reverse = function() {
     return this.split('').reverse().join('');
 };
 
-var num = 11;
-while (true) {
-    if ((num.toString() == num.toString().reverse()) && (num.toString(2) == num.toString(2).reverse()) && (num.toString(8) == num.toString(8).reverse())) {
-        break
+var Palindrome = {
+    start: function() {
+        var num = 11;
+        while (true) {
+            if ((num.toString() == num.toString().reverse()) && (num.toString(2) == num.toString(2).reverse()) && (num.toString(8) == num.toString(8).reverse())) {
+                break
+            }
+            num += 2
+        }
+        console.log(num);
     }
-    num += 2
-}
+};
 
-console.log(num);
+Palindrome.start();
