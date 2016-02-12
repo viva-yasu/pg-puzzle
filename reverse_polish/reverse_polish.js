1ae775ce617dc4834e629795c8372dd744d8e3fa
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-13 02:09:59 +0900

    refactoring

--- reverse_polish/reverse_polish.js
+++ reverse_polish/reverse_polish.js
@@ -10,10 +10,11 @@ var ReversePolish = {
             for (var j = 0, len = this.op.length; j < len; j++) {
                 for (var k = 0; k < len; k++) {
                     for (var l = 0; l < len; l++) {
-                        var val = Number(c.charAt(3)) + this.op[j] + Number(c.charAt(2)) + this.op[k] + Number(c.charAt(1)) + this.op[l] + Number(c.charAt(0));
+                        var val = Number(c.charAt(0)) + this.op[j] + Number(c.charAt(1)) + this.op[k] + Number(c.charAt(2)) + this.op[l] + Number(c.charAt(3));
                         if (val.length > 4) {
-                            if (i == eval(val)) {
-                                console.log(c.reverse() + " → " + val + " = " + i);
+                            var result = eval(val);
+                            if (result == c.reverse()) {
+                                console.log(c + " → " + val + " = " + result);
                             }
                         }
                     }
