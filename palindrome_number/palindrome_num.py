e95a2d673292377ab461e85cc3cfb74f4fa9a95e
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-14 18:24:15 +0900

    refactoring python

--- palindrome_number/palindrome_num.py
+++ palindrome_number/palindrome_num.py
@@ -1,10 +1,13 @@
-num = 11
-while True:
-    c = str(num)
-    b = str(bin(num))[2:]
-    o = str(oct(num))[1:]
-    if c == c[::-1] and b == b[::-1] and o == o[::-1]:
-        break
-    num += 2
+def palindrome():
+    num = 11
+    while True:
+        c = str(num)
+        b = str(bin(num))[2:]
+        o = str(oct(num))[1:]
+        if c == c[::-1] and b == b[::-1] and o == o[::-1]:
+            break
+        num += 2
 
-print num
+    print num
+
+palindrome()
